65846f5b8152fe6920d6f38c3b16edc193e95964
Vijayroman <42169869+Vijayroman@users.noreply.github.com>
2019-07-03 18:38:15 +0530

    Create pro8.py

---
+++ pro8.py
@@ -0,0 +1,11 @@
+import math
+var,sub=map(int,input().split())
+cc=[]
+ken=list(map(int,input().split()))
+for i in range(0,sub):
+    l,h=map(int,input().split())
+    cc.append([l,h])
+for i in cc:
+    d=i[0]-1
+    e=i[1]-1
+    print(math.gcd(ken[d],ken[e]))
